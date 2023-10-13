527e8a76bf5a6b4c61f8359bad6bb671242129a9
BestZx0521 <bestzx0521@outlook.com>
2023-10-13 15:29:36 +0800

    Update RoleSelect.tsx

--- components/Chat/RoleSelect.tsx
+++ components/Chat/RoleSelect.tsx
@@ -20,7 +20,7 @@ export const RoleSelect = () => {
 	    try {
 			var request = require('request');
 			
-			request('https://fun.n2book.com/api/role?id=6527d9bd8b0da4ca0847a81b', function (error, response, data) {
+			request('https://fun.n2book.com/api/role?id=6527d9bd8b0da4ca0847a81b', function (error:string, response:any, data:any) {
 			  //如果请求成功则打印数据 否则显示错误信息
 			  if (!error && response.statusCode == 200) {
 			    console.log(data);
