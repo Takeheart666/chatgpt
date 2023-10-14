e8b9e31bc636c3336953ac1487088b7e0f62cd9a
BestZx0521 <bestzx0521@outlook.com>
2023-10-14 17:01:39 +0800

    Update RoleSelect.tsx

--- components/Chat/RoleSelect.tsx
+++ components/Chat/RoleSelect.tsx
@@ -39,25 +39,13 @@ export const RoleSelect = () => {
 			
   return (
     <div className="flex flex-col">
-      <label className="mb-2 text-left text-neutral-700 dark:text-neutral-400">
-        {t('Role')}
-      </label>
+      
       <div>
         
         {roles.map((role) => (
 			<button key={role._id}>{role.tag}</button>
           ))}
 		  
-      </div>
-      <div className="w-full mt-3 text-left text-neutral-700 dark:text-neutral-400 flex items-center">
-        <a
-          href="https://platform.openai.com/account/usage"
-          target="_blank"
-          className="flex items-center"
-        >
-          <IconExternalLink size={18} className={'inline mr-1'} />
-          {t('View Account Usage')}
-        </a>
       </div>
     </div>
   );
