931c0f0bf5b2d48eabc8cf550b01803ac0f84176
BestZx0521 <bestzx0521@outlook.com>
2023-10-17 14:33:29 +0800

    Update SystemPrompt.tsx

--- components/Chat/SystemPrompt.tsx
+++ components/Chat/SystemPrompt.tsx
@@ -1,133 +1,3 @@
-import {
-  FC,
-  KeyboardEvent,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-  useContext,
-} from 'react';
-
-import { useTranslation } from 'next-i18next';
-
-import { DEFAULT_SYSTEM_PROMPT } from '@/utils/app/const';
-
-import { Conversation } from '@/types/chat';
-import { Prompt } from '@/types/prompt';
-
-
-import HomeContext from '@/pages/api/home/home.context';
-import { KeyValuePair } from '@/types/data';
-
-import { PromptList } from './PromptList';
-import { VariableModal } from './VariableModal';
-
-
-interface Props {
-  conversation: Conversation;
-  prompts: Prompt[];
-  onChangePrompt: (prompt: string) => void;
-}
-
-export const SystemPrompt: FC<Props> = ({
-  conversation,
-  prompts,
-  onChangePrompt,
-}) => {
-  const { t } = useTranslation('chat');
-  const [value, setValue] = useState<string>('');
-  const [activePromptIndex, setActivePromptIndex] = useState(0);
-  const [showPromptList, setShowPromptList] = useState(false);
-  const [promptInputValue, setPromptInputValue] = useState('');
-  const [variables, setVariables] = useState<string[]>([]);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const promptListRef = useRef<HTMLUListElement | null>(null);
-
-  const filteredPrompts = prompts.filter((prompt) =>
-    prompt.name.toLowerCase().includes(promptInputValue.toLowerCase()),
-  );
-  const {
-    state: { selectedConversation, models, roles, defaultModelId, defaultRoleId },
-    handleUpdateConversation,
-    dispatch: homeDispatch,
-  } = useContext(HomeContext);
-
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const value = e.target.value;
-    const maxLength = conversation.model.maxLength;
-
-    if (value.length > maxLength) {
-      alert(
-        t(
-          `Prompt limit is {{maxLength}} characters. You have entered {{valueLength}} characters.`,
-          { maxLength, valueLength: value.length },
-        ),
-      );
-      return;
-    }
-
-    setValue(value);
-    updatePromptListVisibility(value);
-
-    if (value.length > 0) {
-      onChangePrompt(value);
-    }
-  };
-
-  const handleInitModal = () => {
-    const selectedPrompt = filteredPrompts[activePromptIndex];
-    setValue((prevVal) => {
-      const newContent = prevVal?.replace(/\/\w*$/, selectedPrompt.content);
-      return newContent;
-    });
-    handlePromptSelect(selectedPrompt);
-    setShowPromptList(false);
-  };
-
-  const parseVariables = (content: string) => {
-    const regex = /{{(.*?)}}/g;
-    const foundVariables = [];
-    let match;
-
-    while ((match = regex.exec(content)) !== null) {
-      foundVariables.push(match[1]);
-    }
-
-    return foundVariables;
-  };
-
-  const updatePromptListVisibility = useCallback((text: string) => {
-    const match = text.match(/\/\w*$/);
-
-    if (match) {
-      setShowPromptList(true);
-      setPromptInputValue(match[0].slice(1));
-    } else {
-      setShowPromptList(false);
-      setPromptInputValue('');
-    }
-  }, []);
-
-  const handlePromptSelect = (prompt: Prompt) => {
-    const parsedVariables = parseVariables(prompt.content);
-    setVariables(parsedVariables);
-
-    if (parsedVariables.length > 0) {
-      setIsModalVisible(true);
-    } else {
-      const updatedContent = value?.replace(/\/\w*$/, prompt.content);
-
-      setValue(updatedContent);
-      onChangePrompt(updatedContent);
-
-      updatePromptListVisibility(prompt.content);
-    }
-  };
-  
-  
-
   const handleSubmit = (updatedVariables: string[]) => {
     const newContent = value?.replace(/{{(.*?)}}/g, (match, variable) => {
       const index = variables.indexOf(variable);
@@ -181,10 +51,8 @@ export const SystemPrompt: FC<Props> = ({
   useEffect(() => {
     if (conversation.prompt) {
       setValue(conversation.prompt);
-	  console.log('conversation.prompt:'+conversation.prompt)
     } else {
       setValue(DEFAULT_SYSTEM_PROMPT);
-	  console.log('DEFAULT_SYSTEM_PROMPT')
     }
   }, [conversation]);
 
@@ -204,17 +72,9 @@ export const SystemPrompt: FC<Props> = ({
       window.removeEventListener('click', handleOutsideClick);
     };
   }, []);
-  
-  
-  const onAlert = (roleContent: string) => {
-    // 更新状态变量的值
-    setValue(roleContent);
-	onChangePrompt(roleContent);
-  };
 
   return (
     <div className="flex flex-col">
-	
 		<div>
 		  {roles.map((role) => (
 		    <button
@@ -226,7 +86,6 @@ export const SystemPrompt: FC<Props> = ({
 		    </button>
 		  ))}
 		</div>
-	
       <label className="mb-2 text-left text-neutral-700 dark:text-neutral-400">
         {t('System Prompt')}
       </label>
